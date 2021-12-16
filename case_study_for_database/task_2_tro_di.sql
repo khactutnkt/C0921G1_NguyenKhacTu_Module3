6dc9150ca63da0e6c9f4a831b3805e95fcf801f7
khactutnkt <khactut@gmail.com>
2021-12-16 17:03:53 +0700

    16/12/2021

--- case_study_for_database/task_2_tro_di.sql
+++ case_study_for_database/task_2_tro_di.sql
@@ -83,6 +83,11 @@ union
 select ho_ten
 from khach_hang;
 
+-- cách 3
+select distinct ho_ten
+from khach_hang;
+
+
 /* 9. Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng
 trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng. 
 */
@@ -250,3 +255,53 @@ from nhan_vien
 union all
 select ma_khach_hang id, ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi
 from khach_hang;
+
+/* 21. Tạo khung nhìn có tên là v_nhan_vien để lấy được thông tin của tất cả
+các nhân viên có địa chỉ là “Đà Nẵng” và đã từng lập hợp đồng cho một
+hoặc nhiều khách hàng bất kì với ngày lập hợp đồng là “12/12/2019”. 
+*/
+create view v_nhan_vien as
+select nv.*
+from nhan_vien nv
+	join hop_dong hd on nv.ma_nhan_vien = hd.ma_nhan_vien
+where dia_chi like '%Đà Nẵng%' and ngay_lam_hop_dong = '2019-12-12';
+select * from v_nhan_vien;
+
+
+/* 22. Thông qua khung nhìn v_nhan_vien thực hiện cập nhật địa chỉ thành “Liên Chiểu” 
+đối với tất cả các nhân viên được nhìn thấy bởi khung nhìn này. 
+*/
+
+/* 23. Tạo Stored Procedure sp_xoa_khach_hang dùng để xóa thông tin của
+một khách hàng nào đó với ma_khach_hang được truyền vào như là 1 tham số của sp_xoa_khach_hang. 
+*/
+delimiter //
+create procedure sp_xoa_khach_hang(ma_khach_hang int)
+begin
+	delete from khach_hang
+    where khach_hang.ma_khach_hang = ma_khach_hang;
+end;
+// delimiter ;
+call sp_xoa_khach_hang(8);
+
+/* 24. Tạo Stored Procedure sp_them_moi_hop_dong dùng để thêm mới vào bảng hop_dong với yêu cầu 
+sp_them_moi_hop_dong phải thực hiện kiểm tra tính hợp lệ của dữ liệu bổ sung, với nguyên tắc 
+không được trùng khóa chính và đảm bảo toàn vẹn tham chiếu đến các bảng liên quan. 
+*/
+-- delimiter //
+-- create procedure sp_them_moi_hop_dong
+-- begin
+-- 	
+-- end;
+-- // delimiter ;
+
+
+
+
+
+
+
+
+
+
+
