287a2d5e1ae802d4d118431a3c361885afbb6bef
khactutnkt <khactut@gmail.com>
2021-12-26 19:03:11 +0700

    26/12/2021

--- ss10_jsp_jstl/bai_tap/calculator/src/main/java/CalculatorServlet.java
+++ ss10_jsp_jstl/bai_tap/calculator/src/main/java/CalculatorServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "calculatorServlet", urlPatterns = {"","/calculator"})
-public class calculatorServlet extends HttpServlet {
+public class CalculatorServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String number1 = request.getParameter("firstOperand");
@@ -14,21 +14,21 @@ public class calculatorServlet extends HttpServlet {
         String operand = request.getParameter("operand");
         Double firstOperand = Double.parseDouble(number1);
         Double secondOperand = Double.parseDouble(number2);
-        Double result_calculator;
+        Double resultCalculator;
         try {
             switch (operand){
                 case "+":
-                    result_calculator = firstOperand + secondOperand;
+                    resultCalculator = firstOperand + secondOperand;
                     break;
                 case "-":
-                    result_calculator = firstOperand - secondOperand;
+                    resultCalculator = firstOperand - secondOperand;
                     break;
                 case "*":
-                    result_calculator = firstOperand * secondOperand;
+                    resultCalculator = firstOperand * secondOperand;
                     break;
                 case "/":
                     if (secondOperand != 0){
-                        result_calculator = firstOperand / secondOperand;
+                        resultCalculator = firstOperand / secondOperand;
                     }
                     else {
                         throw new RuntimeException("Error in division by 0");
@@ -37,7 +37,7 @@ public class calculatorServlet extends HttpServlet {
                 default:
                     throw new RuntimeException("Invalid operand");
             }
-            String result = number1 + " " + operand + " " + number2 + " = " + result_calculator;
+            String result = number1 + " " + operand + " " + number2 + " = " + resultCalculator;
             request.setAttribute("result", result);
             request.getRequestDispatcher("result_calculator.jsp").forward(request,response);
         }catch (Exception e){
